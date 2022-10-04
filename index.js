b91b4da1d926dea906752ee770542d5773168f19
emmanuel <ogahemmanuel75@gmail.com>
2022-10-04 21:42:11 +0100

    Setup router API

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const connectDB = require('./config/db')
 const app = express();
-const port = 3000;
+
+connectDB();
+
+app.use(express.json({extended: false}))
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Welcome to Afara initiative project");
 });
 
+app.use('api/users', require('./routes/api/users'))
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
