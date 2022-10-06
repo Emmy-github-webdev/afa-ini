7be4d1e55d3dce8a096b302799f9c30f732279fc
emmanuel <ogahemmanuel75@gmail.com>
2022-10-06 09:30:36 +0100

    Update validation

--- meddleware/validation.js
+++ meddleware/validation.js
@@ -1,13 +1,20 @@
-const { check } = require('express-validator');
+// const {check} = require('express-validator');
 
+exports.userSignupValidator = (req, res, next) => {
+  req.check('name', 'Name is required').notEmpty()
+  req.check('email', 'It must be a valid email')
+      .matches(/.+\@.+\..+/)
+      .withMessage('Email must contain @')
+      .isLength({
+          min: 4, 
+          max: 32
+      });
+      const errors = req.validationErrors();
+      if(errors){
+          const firstError = errors.map(error => error.msg)[0];
+          return res.status(400).json({error: firstError});
+      }
 
-exports.formValidator = (req, res) => [
-    check('name')
-      .not()
-      .isEmpty()
-      .withMessage('Name is required'),
+      next();
 
-    check('email')
-      .isEmail()
-      .withMessage('Must be valid email address'),
-];
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {signup, getUsers} = require('../../controllers/userController');
-const {formValidator} = require('../../meddleware/validation');
+const { userSignupValidator } = require('../../meddleware/validation');
 
-router.post("/", formValidator, signup);
+router.post("/", userSignupValidator, signup);
 router.get("/", getUsers)
 
 module.exports = router;
